0b158600111191d9326d14daf7a7ee10e35ac7be
pradip-dev-branch <sheta.pradip@gmail.com>
2017-05-04 15:12:22 +0530

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController, IonicPage } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+ 
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  username = '';
+  email = '';
+  info : any = {};
+  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController,) {
+    this.info = this.auth.getUserInfo();
+    console.log(' info ' + JSON.stringify(this.info));
+    
+    if(this.info == null){
+      this.showError('Please login first');
+      this.nav.setRoot('LoginPage');
+    }
+    // this.showError(JSON.stringify(info));
+  }
+ 
+  public logout() {
+    this.auth.logout().subscribe(succ => {
+      this.nav.setRoot('LoginPage');
+    });
+  }
+
+  showError(text) {
+ 
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
+}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController, LoadingController, Loading, IonicPage } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { AuthService } from '../../providers/auth-service';
+ 
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  loading: Loading;
+  registerCredentials = { email: '', password: '', type: 'company' };
+  typeList: any[] = [{ value: 1, text: 'option 1', checked: false }, { value: 2, text: 'option 2', checked: false }];
+ 
+  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController, private loadingCtrl: LoadingController, private http: Http) { 
+  }
+ 
+  public createAccount() {
+    this.nav.push('RegisterPage');
+  }
+ 
+  public login() {
+    this.showLoading();
+    let body = new FormData();
+        body.append('email', this.registerCredentials.email);
+        body.append('password', this.registerCredentials.password);
+        body.append('type', this.registerCredentials.type);
+        let headers = new Headers({});
+        let options = new RequestOptions({ headers: headers });
+        console.log(' this.registerCredentials ' + this.registerCredentials);
+        // this.showError(JSON.stringify(this.registerCredentials));
+        this.http
+            .post('http://192.169.176.227/backofficeweb/', body, options)
+            .map(res => res.json())
+            .subscribe(
+                data => {
+                  console.log('login data  ' + data);
+                  if(data.status == 'Success'){
+                    this.auth.setCurrentUser(data.Data, this.registerCredentials.email);
+                    this.nav.setRoot('HomePage');
+                  } else {
+                    this.showError(data.message);
+                  }
+                },
+                err => {
+                  console.log("ERROR!: ", err);
+                }
+            );
+
+    // this.auth.login(this.registerCredentials, this.http).subscribe(allowed => {
+    //   if (allowed) {        
+    //     this.nav.setRoot('HomePage');
+    //   } else {
+    //     this.showError("Access Denied");
+    //   }
+    // },
+    //   error => {
+    //     this.showError(error);
+    //   });
+  }
+ 
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
+ 
+  showError(text) {
+    this.loading.dismiss();
+ 
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
+}
